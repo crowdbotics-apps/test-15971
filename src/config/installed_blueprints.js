c048c5e4c3fba9000789f9a7c1a8a490c82f3e16
Crowdbotics <team@crowdbotics.com>
2020-04-28 02:47:30 +0000

    add package blueprint-react-native-notifications-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'NotificationList33928', human_name: 'Notification List', access_route: 'NotificationList33928'},
 { name: 'Maps33927', human_name: 'Maps', access_route: 'Maps33927', icon: 'map'},
 { name: 'Messenger', human_name: 'Messenger', access_route: 'Messenger', icon: 'comments'},
 { name: 'Tutorial', human_name: 'Tutorial', access_route: 'Tutorial', icon: 'question-circle'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import NotificationList33928Navigator from '../features/NotificationList33928/navigator';
 import Maps33927Navigator from '../features/Maps33927/navigator';
 import MessengerNavigator from '../features/Messenger/navigator';
 import TutorialNavigator from '../features/Tutorial/navigator';
@@ -22,6 +23,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+NotificationList33928: { screen: NotificationList33928Navigator },
 Maps33927: { screen: Maps33927Navigator },
 Messenger: { screen: MessengerNavigator },
 Tutorial: { screen: TutorialNavigator },
